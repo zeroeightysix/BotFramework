9870a098a005c8652ef0a6553a39bb83933b261c
zeroeightsix <RidanVandenbergh@gmail.com>
2018-02-25 19:06:07 +0100

    Example bot now uses the new configuration

--- BotExample/src/me/zeroeightsix/bot/Bot.java
+++ BotExample/src/me/zeroeightsix/bot/Bot.java
@@ -1,15 +1,11 @@
 package me.zeroeightsix.bot;
 
-import me.zeroeightsix.botframework.bcfg.InvalidConfigurationException;
-import me.zeroeightsix.botframework.bcfg.file.YamlBasicConfiguration;
+import me.zeroeightsix.botframework.event.CommandEvent;
 import me.zeroeightsix.botframework.plugin.EventHandler;
 import me.zeroeightsix.botframework.plugin.Plugin;
 import me.zeroeightsix.botframework.plugin.command.ChatCommand;
-import me.zeroeightsix.botframework.event.CommandEvent;
 import me.zeroeightsix.botframework.plugin.command.IChatCommand;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.EventListener;
 
 /**
@@ -17,10 +13,8 @@ import java.util.EventListener;
  */
 public class Bot extends Plugin implements EventListener {
 
-    YamlBasicConfiguration configuration;
-
     public Bot() {
-        super("ExampleBot", "1.1", "An example bot plugin for botframework");
+        super("ExampleBot", "1.2", "An example bot plugin for botframework");
     }
 
     @Override
@@ -51,18 +45,13 @@ public class Bot extends Plugin implements EventListener {
             }
         }, this));
 
-        // Load a (yml) bcfg. This uses YAML, however there is also a "JsonConfiguration" object (botframework.cfg.JsonConfiguration) which uses JSON
-        configuration = new YamlBasicConfiguration();
-        try {
-            configuration.load(new File(getDataFolder(), "myConfiguration.yml"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InvalidConfigurationException e) {
-            e.printStackTrace();
-        }
+        // Usage of the configuration that comes with every plugin:
+        getConfiguration().setString("hello.world", "Hello world!");
+        // Basic method for getting a string from the config.
+        getLogger().info("The value of 'hello.world' is: " + getConfiguration().getString("hello.world"));
+        // And of course, we'd like to save our changes to the configuration:
+        getConfiguration().save();
 
-        // Basic method for getting a string from the bcfg.
-        getLogger().info("The value of 'helloworld' in the bcfg is: " + configuration.get("helloworld"));
     }
 
     // Event called when a player executes a command
